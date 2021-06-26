79da5748aa56dec1cc4d691afdbf7ae331728b32
Sohan Ahmed <82075290+asohaahmed@users.noreply.github.com>
2021-06-27 05:30:43 +0600

    learn js

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+var sohan = 1.5678;
+
+
+
+//sohan = toString(sohan);
+document.write(typeof(sohan));
+
+
+//console.log(sohan.toFixed(1))
+
+console.log(sohan.toPrecision())
+
+document.write(  typeof( Number("45")))
+
+
+
+
+
+
+/*
+
+var name = "4564";
+document.write(typeof(name))
+
+console.log(name)
+var name,age,country;
+name ="sohan <br/>";
+age ="25  <br/>";
+country ="bangladesh"
+document.write(name,age,country);
+document.write(<br/> + typeof(name));
+console.log(typeof(name));
+document.write(typeof(name))
+*/
+
